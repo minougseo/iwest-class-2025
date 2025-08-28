7f9b59cc343936580b9c9efcdd03d27347c4073e
Minoug Seo <oalsdnro72@gmail.com>
2025-08-28 10:33:16 +0900

    good

--- utils.py
+++ utils.py
@@ -1,6 +1,27 @@
 
 import os
 import requests
+from openai import OpenAI
+from openai.types.shared.chat_model import ChatModel
+
+def make_response(
+        user_content: str,
+        system_content: str | None = None,
+        model: str | ChatModel = "gpt-4o-mini",
+        temperature: float = 0.25,
+        api_key: str | None = None,
+) -> str:
+    messages =[]
+    if system_content: # 빈 문자열도 아니고, None 도 아닐 때
+        messages.append({"role": "system", "content": system_content})
+    messages.append({"role": "user", "content": user_content})
+    client = OpenAI(api_key=api_key)
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=temperature,
+    )
+    return response.choices[0].message.content
 
 def download_file(
         file_url: str,
